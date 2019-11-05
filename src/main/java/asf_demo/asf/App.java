04e0a2f0ad89593dd07d70f44ffd4525bc31a895
Ketki <ketki.thosar@acellere.com>
2019-11-05 13:01:27 +0530

    ASF:1081

---
+++ src/main/java/asf_demo/asf/App.java
@@ -0,0 +1,13 @@
+package asf_demo.asf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
